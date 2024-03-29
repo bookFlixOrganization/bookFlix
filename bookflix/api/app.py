45fbcd80955304d43f3c4c65b2cb5de9e8a8fc21
Artem <khnartem@gmail.com>
2024-03-29 22:38:55 +0300

    App start without docker

--- bookflix/api/app.py
+++ bookflix/api/app.py
@@ -9,3 +9,12 @@ def create_app():
     )
 
     return app
+
+
+def run_server(app=FastAPI(
+    debug=True,
+    docs_url='/api/docs',
+    title='Test'
+)):
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
