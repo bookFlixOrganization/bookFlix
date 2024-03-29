9dd01ac16e336b1a2abb2de23d183d19ac923dac
zmskv <zmskvxd@gmail.com>
2024-03-29 20:00:52 +0300

    rebase project

--- bookflix/db/models.py
+++ bookflix/db/models.py
@@ -1,14 +1,14 @@
 import uuid
 
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 
 Base = declarative_base()
 
 
 def User(Base):
-    __tablename__ = "users"
+    pass
 
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
--- bookflix/db/session.py
+++ bookflix/db/session.py
@@ -1,7 +1,6 @@
-from common.settings import DATABASE_URL
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
+from sqlalchemy.orm import sessionmaker
+from common.settings import DATABASE_URL
 
 engine = create_engine(url=DATABASE_URL, echo=True)
 new_session = sessionmaker(engine, expire_on_commit=False)
