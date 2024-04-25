38acf8b0c5f594cf0fa7ca542e76b39c2fd0696e
aazaretskaya <nastya.zaretskaya.1995@gmail.com>
2024-04-25 23:32:52 +0300

    Improve reading diary

---
+++ Whole_article/js/whole_article.js
@@ -0,0 +1,15 @@
+const icon = document.querySelector('.icon');
+const search = document.querySelector('.search');
+const clear = document.querySelector('.clear');
+
+icon.onclick = function() {
+    search.classList.toggle('active');
+};
+
+clear.onclick = function() {
+    document.getElementById("mySearch").value = '';
+};
+
+
+
+
