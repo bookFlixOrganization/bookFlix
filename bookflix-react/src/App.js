417edae6287fddcfc7920408abc386dd81ae3dab
mihateren <mihateren2003@gmail.com>
2024-05-01 14:21:26 +0300

    add MainPage in Router

--- bookflix-react/src/App.js
+++ bookflix-react/src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage.jsx';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" />
+                <Route path="/" element={<MainPage />} />
             </Routes>
         </div>
     );
