f4adbc76dab4766a6192ff0c541f497b1b586b65
mihateren <mihateren2003@gmail.com>
2024-05-01 15:05:40 +0300

    add MainPage.module.css styles

--- bookflix-react/src/components/MainPage/MainPage.jsx
+++ bookflix-react/src/components/MainPage/MainPage.jsx
@@ -3,11 +3,12 @@ import YourFilms from './YourFilms.jsx';
 import YourBooks from './YourBooks.jsx';
 import PopularFilms from './PopularFilms.jsx';
 import PopularBooks from './PopularBooks.jsx';
+import styles from './MainPage.module.css';
 
 const MainPage = () => {
     return (
         <div className="page-container">
-            <h2>Главная</h2>
+            <h2 className={styles.main_title}>Главная</h2>
             <YourFilms />
             <YourBooks />
             <PopularFilms />
