edfbcfe841943d9e3fa62825bc7a8155e015019b
mihateren <mihateren2003@gmail.com>
2024-05-02 17:07:54 +0300

    init all films

--- bookflix-react/src/App.js
+++ bookflix-react/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import MainPageContainer from './components/MainPage/MainPageContainer.jsx';
 import HeaderContainer from './components/Header/HeaderContainer.jsx';
 import AuthPageContainer from './components/AuthPage/AuthPageContainer.jsx';
+import AllFilmsContainer from './components/AllFilms/AllFilmsContainer.jsx';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainPageContainer />} />
                 <Route path="auth" element={<AuthPageContainer />} />
+                <Route path="all-films" element={<AllFilmsContainer />} />
             </Routes>
         </div>
     );
