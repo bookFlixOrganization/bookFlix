5932ec75a6be176e2fa98faba291e27741fab99c
mihateren <mihateren2003@gmail.com>
2024-05-06 12:10:59 +0300

    add genres items

--- bookflix-react/src/components/Preferences/Preferences.jsx
+++ bookflix-react/src/components/Preferences/Preferences.jsx
@@ -10,9 +10,107 @@ const Preferences = () => {
             <div className={styles.preferences_genres}>
                 <div className={styles.preferences_item}>
                     <div className={styles.preferences_item_emoji}>😱</div>
-                    <div className={styles.preferences_item_name}>Horror</div>
+                    <div className={styles.preferences_item_name}>Хоррор</div>
+                    {/* Horror */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🥺</div>
+                    <div className={styles.preferences_item_name}>Драма</div>
+                    {/* Drama */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>👻</div>
+                    <div className={styles.preferences_item_name}>Мистика</div>
+                    {/* Mystery */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🤖</div>
+                    <div className={styles.preferences_item_name}>Научно-фантастический</div>
+                    {/* Sci-fi */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🍷</div>
+                    <div className={styles.preferences_item_name}>Романтический</div>
+                    {/* Romance */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🖤</div>
+                    <div className={styles.preferences_item_name}>Нуар</div>
+                    {/* Film-noir */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🗡️</div>
+                    <div className={styles.preferences_item_name}>Фентези</div>
+                    {/* Fantasy */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🗿</div>
+                    <div className={styles.preferences_item_name}>Биография</div>
+                    {/* Biography */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🗿</div>
+                    <div className={styles.preferences_item_name}>Биография</div>
+                    {/* Biography */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>📄</div>
+                    <div className={styles.preferences_item_name}>Документальный</div>
+                    {/* Documentary */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🪩</div>
+                    <div className={styles.preferences_item_name}>Мюзикл</div>
+                    {/* Music */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🙀</div>
+                    <div className={styles.preferences_item_name}>Триллер</div>
+                    {/* Thriller */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🔫</div>
+                    <div className={styles.preferences_item_name}>Криминальный</div>
+                    {/* Crime */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>💥</div>
+                    <div className={styles.preferences_item_name}>Экшн</div>
+                    {/* Action */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🪖</div>
+                    <div className={styles.preferences_item_name}>Военный</div>
+                    {/* War */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🤠</div>
+                    <div className={styles.preferences_item_name}>Приключения</div>
+                    {/* Adventure */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>😂</div>
+                    <div className={styles.preferences_item_name}>Комедия</div>
+                    {/* Comedy */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🌵</div>
+                    <div className={styles.preferences_item_name}>Вестерн</div>
+                    {/* Western */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>🏛️</div>
+                    <div className={styles.preferences_item_name}>Исторический</div>
+                    {/* History */}
+                </div>
+                <div className={styles.preferences_item}>
+                    <div className={styles.preferences_item_emoji}>👨‍👩‍👦</div>
+                    <div className={styles.preferences_item_name}>Семейный</div>
+                    {/* Семейный */}
                 </div>
             </div>
+            <h2 className={styles.preferences_title}>Давайте выберем ваши предпочтения в книгах</h2>
+            <button className={styles.prefenres_button}>Готово</button>
         </div>
     );
 };
