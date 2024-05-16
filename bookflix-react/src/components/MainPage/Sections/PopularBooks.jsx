fc8544a0489a17d4555928d585346fdd3163f446
mihateren <mihateren2003@gmail.com>
2024-05-16 21:28:35 +0300

    +- fix slider

--- bookflix-react/src/components/MainPage/Sections/PopularBooks.jsx
+++ bookflix-react/src/components/MainPage/Sections/PopularBooks.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Section.module.css';
 import { NavLink } from 'react-router-dom';
 
 const PopularBooks = (props) => {
-    console.log(props);
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [slidesPerView, setSlidesPerView] = useState(5); // eslint-disable-line
+
+    const nextSlide = () => {
+        setCurrentSlide((currentSlide + 1) % props.popularBooks.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentSlide((currentSlide - 1 + props.popularBooks.length) % props.popularBooks.length);
+    };
+
+    const startIndex = currentSlide * slidesPerView;
+
     return (
         <section className={styles.now_reading_text}>
             <div className={styles.container}>
@@ -18,43 +30,58 @@ const PopularBooks = (props) => {
                         Самые популярные книги за последний час
                     </p>
                 </div>
-                <div className={styles.swiper}>
-                    <div className={styles.swiper_wrapper}>
-                        {props.popularBooks &&
-                            props.popularBooks.map((book, index) => (
-                                <div key={`popular_book_${index}`} className={styles.swiper_slide}>
-                                    <div className={styles.container}></div>
-                                    <div className={styles.movies}>
-                                        <a href={book.buy_links[0].url} className={styles.movie}>
-                                            <div className={styles.movie__cover_inner}>
-                                                <img
-                                                    src={book.book_image}
-                                                    alt={book.title}
-                                                    className={styles.movie__cover}
-                                                />
-                                                <div className={styles.movie__cover_darkened}></div>
+                <div className={styles.swiper_container}>
+                    <button onClick={prevSlide} className={styles.swiper_button_prev}>
+                        &#10094;
+                    </button>
+                    <div className={styles.swiper}>
+                        <div className={styles.swiper_wrapper}>
+                            {props.popularBooks &&
+                                props.popularBooks.map((book, index) => {
+                                    const isVisible =
+                                        index >= startIndex && index < startIndex + slidesPerView;
+                                    return (
+                                        <div
+                                            key={`popular_book_${index}`}
+                                            className={`${styles.swiper_slide} ${isVisible ? '' : styles.hidden}`}
+                                        >
+                                            <div className={styles.container}></div>
+                                            <div className={styles.movies}>
+                                                <a
+                                                    href={book.buy_links[0].url}
+                                                    className={styles.movie}
+                                                >
+                                                    <div className={styles.movie__cover_inner}>
+                                                        <img
+                                                            src={book.book_image}
+                                                            alt={book.title}
+                                                            className={styles.movie__cover}
+                                                        />
+                                                        <div
+                                                            className={styles.movie__cover_darkened}
+                                                        ></div>
+                                                    </div>
+                                                    <div className={styles.movie__info}>
+                                                        <div className={styles.movie__title}>
+                                                            {book.title}
+                                                        </div>
+                                                        <div className={styles.movie__category}>
+                                                            {book.author}
+                                                        </div>
+                                                        <div className={styles.movie__average}>
+                                                            {book.rank}
+                                                        </div>
+                                                    </div>
+                                                </a>
                                             </div>
-                                            <div className={styles.movie__info}>
-                                                <div className={styles.movie__title}>
-                                                    {book.title}
-                                                </div>
-                                                <div className={styles.movie__category}>
-                                                    {book.author}
-                                                </div>
-                                                <div className={styles.movie__average}>
-                                                    {book.rank}
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            ))}
-                        <NavLink to="/">
-                            <div className={styles.movie_show_all}>
-                                <p className={styles.show_all}>Смотреть всё</p>
-                            </div>
-                        </NavLink>
+                                        </div>
+                                    );
+                                })}
+                        </div>
                     </div>
+                    <button onClick={nextSlide} className={styles.swiper_button_next}>
+                        &#10095;
+                    </button>
                 </div>
             </div>
         </section>
