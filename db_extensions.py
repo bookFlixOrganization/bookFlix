fe885182fb50ccb41f89dfec624622853cd4ac5f
cbf <crazyblackfire@mail.ru>
2024-05-25 15:39:09 +0300

    fix installing extensions before: using psycopg2, now: using asyncpg

--- db_extensions.py
+++ db_extensions.py
@@ -2,43 +2,51 @@
 Скрипт создания расширеня uuid-ossp в базах данных
 """
 
-import psycopg2
+import asyncio
+import asyncpg
 
 from src.config.project_config import settings
 
 
-def install_uuid_extension(name, host, user, password):
+async def install_uuid_extension(pool, dbname):
     """
-    Подключение к базе и создание в ней расширение
+    Создание расширения в переданной базе данных
     """
-    try:
-        connection = psycopg2.connect(
-            dbname=name, user=user, password=password, host=host
-        )
-        cursor = connection.cursor()
+    async with pool.acquire() as connection:
+        try:
+            await connection.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+            print(f"Расширение uuid-ossp установлено в базе данных {dbname}")
+        except asyncpg.PostgresError as e:
+            print(f"Ошибка при установке расширения в базе данных {dbname}: {e}")
 
-        cursor.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
 
-        connection.commit()
-        print(f"Расширение uuid-ossp установлено в базе данных {name}")
-    except psycopg2.OperationalError as e:
-        print(f"Ошибка подключения к базе данных {name}: {e}")
-    except psycopg2.ProgrammingError as e:
-        print(f"Ошибка при выполнении SQL-запроса в базе данных {name}: {e}")
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
+async def main():
+    """
+    Создание подключений к базам данных
+    """
+    conn_params_db = {
+        "host": settings.DB_HOST,
+        "user": settings.DB_USER,
+        "password": settings.DB_PASS,
+        "database": settings.DB_NAME,
+    }
+
+    conn_params_testdb = {
+        "host": settings.DB_HOST_TEST,
+        "user": settings.DB_USER_TEST,
+        "password": settings.DB_PASS_TEST,
+        "database": settings.DB_NAME_TEST,
+    }
+
+    pool_db = await asyncpg.create_pool(**conn_params_db)
+    pool_testdb = await asyncpg.create_pool(**conn_params_testdb)
+
+    await install_uuid_extension(pool_db, conn_params_db["database"])
+    await install_uuid_extension(pool_testdb, conn_params_testdb["database"])
+
+    await pool_db.close()
+    await pool_testdb.close()
 
 
 if __name__ == "__main__":
-    install_uuid_extension(
-        settings.DB_NAME, settings.DB_HOST, settings.DB_USER, settings.DB_PASS
-    )
-    install_uuid_extension(
-        settings.DB_NAME_TEST,
-        settings.DB_HOST_TEST,
-        settings.DB_USER_TEST,
-        settings.DB_PASS_TEST,
-    )
+    asyncio.run(main())
