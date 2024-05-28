45df14884127d210ec3e8dafb9c46c8332e5d604
mihateren <mihateren2003@gmail.com>
2024-05-28 15:19:27 +0300

    add navlinks in readingheader

--- bookflix-react/src/components/Reading/ReadingHeader/ReadingHeader.jsx
+++ bookflix-react/src/components/Reading/ReadingHeader/ReadingHeader.jsx
@@ -1,29 +1,69 @@
 import React from 'react';
 import styles from './ReadingHeader.module.css';
+import { NavLink } from 'react-router-dom';
 
-const ReadingHeader = () => {
+const ReadingHeader = (props) => {
     return (
         <div className={styles.reading_diary_container}>
             <section className={styles.now_watching_text}>
                 <div className={styles.container}>
                     <div className={styles.section_header}>
                         <div className={styles.section_header_1}>
-                            <p className={styles.section_title}>Дневник чтения</p>
-                            <a href="/" className={styles.button_all}>
+                            <NavLink
+                                to={'/reading-diary'}
+                                className={
+                                    props.activeLink === 'reading-diary'
+                                        ? styles.active_link
+                                        : styles.button_all
+                                }
+                                onClick={() => props.handleLinkClick('reading-diary')}
+                            >
+                                Дневник чтения
+                            </NavLink>
+                            <NavLink
+                                to={'/popular-articles'}
+                                className={
+                                    props.activeLink === 'popular-articles'
+                                        ? styles.active_link
+                                        : styles.button_all
+                                }
+                                onClick={() => props.handleLinkClick('popular-articles')}
+                            >
                                 Популярные статьи
-                            </a>
-                            <a href="/" className={styles.button_all}>
-                                Мои статьи
-                            </a>
-                            <a href="/" className={styles.button_all}>
+                            </NavLink>
+                            <NavLink
+                                to={'/my-subs'}
+                                className={
+                                    props.activeLink === 'my-subs'
+                                        ? styles.active_link
+                                        : styles.button_all
+                                }
+                                onClick={() => props.handleLinkClick('my-subs')}
+                            >
                                 Мои подписки
-                            </a>
-                            <a href="/" className={styles.button_all}>
+                            </NavLink>
+                            <NavLink
+                                to={'/new-article'}
+                                className={
+                                    props.activeLink === 'new-article'
+                                        ? styles.active_link
+                                        : styles.button_all
+                                }
+                                onClick={() => props.handleLinkClick('new-article')}
+                            >
                                 Новая статья
-                            </a>
-                            <a href="/" className={styles.button_all}>
+                            </NavLink>
+                            <NavLink
+                                to={'/search-article'}
+                                className={
+                                    props.activeLink === 'search-article'
+                                        ? styles.active_link
+                                        : styles.button_all
+                                }
+                                onClick={() => props.handleLinkClick('search-article')}
+                            >
                                 Найти статью
-                            </a>
+                            </NavLink>
                         </div>
                     </div>
                 </div>
--- bookflix-react/src/components/Reading/ReadingHeader/ReadingHeaderContainer.jsx
+++ bookflix-react/src/components/Reading/ReadingHeader/ReadingHeaderContainer.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReadingHeader from './ReadingHeader.jsx';
 
 const ReadingHeaderContainer = () => {
-    return <ReadingHeader />;
+    const [activeLink, setActiveLink] = useState('reading-diary');
+
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+    };
+
+    return <ReadingHeader activeLink={activeLink} handleLinkClick={handleLinkClick} />;
 };
 
 export default ReadingHeaderContainer;
